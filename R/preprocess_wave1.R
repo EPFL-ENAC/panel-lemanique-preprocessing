57c30a973188a04170938bcb95faab58e7b9b66c
Quentin Eschmann <quentin.eschmann@epfl.ch>
2024-11-12 11:05:59 +0100

    feat: added draft script for wave1 preprocessing

---
+++ R/preprocess_wave1.R
@@ -0,0 +1,79 @@
+source(here::here("R/utils.R"))
+
+labels_to_string <- function(index){
+  tryCatch(
+    {
+      obj <- get_labels(wave1_data[index])
+      res = ""
+      for (i in 1:length(row(obj[,"name"]))){
+        temp <- paste(obj[i,"value"], obj[i, "name"], sep=":")
+        if(i == 1){
+          res <- temp
+        }else{
+          res <- paste(res, temp, sep = ", ")
+        }
+      }
+      return(res)
+    },
+    error=function(e){
+      return("")
+    }
+  )
+}
+
+write_cell <- function(file_path, value) {
+  # Get the current column position and total columns
+  current_column <- getOption("current_column")
+  total_columns <- getOption("total_columns")
+  
+  # Choose separator based on whether it's the last cell in the row
+  sep <- if (current_column == total_columns) "\n" else ";"
+  
+  # Write the cell value with the appropriate separator
+  cat(value, sep, file = file_path, append = TRUE)
+  
+  # Update the column counter
+  if (current_column == total_columns) {
+    options(current_column = 1)  # Reset to the first column at the end of a row
+  } else {
+    options(current_column = current_column + 1)  # Move to the next column
+  }
+}
+
+main <- function(){
+  
+  #Gets raw data => needs read access to the LAUSR drive
+  folder <- "//enac1files.epfl.ch/LASUR/common/LaSUR/06 - Recherche/Dossier de travail IT4R/Enquête mobilité"
+  file <- "EPFL_vague1_pond_clean_240319.sav"
+  
+  if (!dir.exists(folder)) {
+    stop(
+      sprintf("Error: dir '%s' is not accessible. ", folder),
+      "Read rights to the LASUR drive are necessary to preprocess the data. "
+    )
+  }
+  
+  wave1_data <- haven::read_sav(file.path(folder, file))
+
+  output_path <- file.path(Sys.getenv("USERPROFILE"), "Documents/Lemanique Panel/Wave_1", "wave1.csv")
+  
+  write.table(t(c("question_code", "question_text", "question_labels")),
+              file = output_path, 
+              sep = ";", 
+              col.names = FALSE,
+              row.names = FALSE, 
+              append = TRUE)
+  options(current_column = 1)
+  options(total_columns = 3)
+  
+  #reads raw data and writes parsed data
+  for (i in 1:length(wave1_data)){
+    question_code <- colnames(wave1_data[i])
+    write_cell(output_path, question_code)
+    write_cell(output_path, gsub("[\r\n]", " ", attr(get(question_code, wave1_data), "label")))
+    write_cell(output_path, labels_to_string(i))
+  }
+}
+
+
+main()
