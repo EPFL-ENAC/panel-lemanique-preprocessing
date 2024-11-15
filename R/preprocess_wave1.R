7989f006e188f3150f63d008e19d14e7e7995b08
Quentin Eschmann <quentin.eschmann@epfl.ch>
2024-11-15 11:39:27 +0100

    perf: rewrote documentation and full_data functions

--- R/preprocess_wave1.R
+++ R/preprocess_wave1.R
@@ -21,75 +21,38 @@ labels_to_string <- function(data, index){
   )
 }
 
-write_cell <- function(file_path, value) {
-  # Get the current column position and total columns
-  current_column <- getOption("current_column")
-  total_columns <- getOption("total_columns")
-  
-  # Choose separator based on whether it's the last cell in the row
-  sep <- if (current_column == total_columns) "\n" else ";"
-  
-  # Write the cell value with the appropriate separator
-  cat(value, sep, file = file_path, append = TRUE)
-  
-  # Update the column counter
-  if (current_column == total_columns) {
-    options(current_column = 1)  # Reset to the first column at the end of a row
-  } else {
-    options(current_column = current_column + 1)  # Move to the next column
-  }
-}
 
 documentation <- function(data){
   output_path <- file.path(Sys.getenv("USERPROFILE"), "Documents/Lemanique Panel/Wave_1", "documentation_wave1.csv")
   
-  write.table(t(c("question_code", "question_text", "question_labels")),
-              file = output_path, 
-              sep = ";", 
-              col.names = FALSE,
-              row.names = FALSE, 
-              append = TRUE)
-  options(current_column = 1)
-  options(total_columns = 3)
-  
-  #reads raw data and writes parsed data
-  for (i in 1:ncol(data)){
-    question_code <- colnames(data[i])
-    write_cell(output_path, question_code)
-    write_cell(output_path, gsub("[\r\n]", " ", attr(get(question_code, data), "label")))
-    write_cell(output_path, labels_to_string(data, i))
+  question_codes <- colnames(wave1_data)
+  question_text <- character(length(question_codes))
+  question_labels <- character(length(question_codes))
+  for (i in seq_along(question_codes)) {
+    question_text[i] <- gsub(
+      "[\r\n]", 
+      " ", 
+      ifelse(is.null(attr(get(question_codes[i], wave1_data), "label")), 
+             NA_character_, 
+             attr(get(question_codes[i], wave1_data), "label"))
+    )
+    
+    question_labels[i] <- labels_to_string(data, i)
   }
+  result <- tibble::tibble(question_code = question_codes, question_text = question_text, question_labels = question_labels)
+  write.csv(result, output_path)
+  
 }
 
-
 full_data <- function(data){
-  
   output_path <- file.path(Sys.getenv("USERPROFILE"), "Documents/Lemanique Panel/Wave_1", "full_data_wave1.csv")
-  
-  
-  
-  write.table(t(colnames(data)),
-              file = output_path, 
-              sep = ";", 
-              col.names = FALSE,
-              row.names = FALSE, 
-              append = TRUE)
-  options(current_column = 1)
-  options(total_columns = length(colnames(data)))
-  options(total_rows = length(rownames(data)))
-  
-  for (i in 1:length(rownames(data))){
-    for (ii in 1:length(colnames(data))){
-      write_cell(output_path, t(data[i,ii]))
-    }
-  }
-  
+  write.csv(data, output_path)
 }
 
 main <- function(){
   
   #Gets raw data => needs read access to the LAUSR drive
-folder <- "//enac1files.epfl.ch/LASUR/common/LaSUR/06 - Recherche/Dossier de travail IT4R/Enquête mobilité"
+  folder <- "//enac1files.epfl.ch/LASUR/common/LaSUR/06 - Recherche/Dossier de travail IT4R/Enquête mobilité"
   file <- "EPFL_vague1_pond_clean_240319.sav"
   
   if (!dir.exists(folder)) {
@@ -103,7 +66,7 @@ folder <- "//enac1files.epfl.ch/LASUR/common/LaSUR/06 - Recherche/Dossier de tra
   
   documentation(wave1_data)
   
-  #full_data(wave1_data)
+  full_data(wave1_data)
 }
 
 
