f30d357e282321d6daed37f1362a1e34d8350d0b
Quentin Eschmann <quentin.eschmann@epfl.ch>
2024-11-18 09:29:55 +0100

    chore: added function documentation for wave1 preprocessing

--- R/preprocess_wave1.R
+++ R/preprocess_wave1.R
@@ -1,5 +1,12 @@
 source(here::here("R/utils.R"))
 
+#' Creates a string from le labels of a question
+#'
+#' @param data SPSS dataset
+#' @param index int representing index if the question
+#' 
+#' @return string
+
 labels_to_string <- function(data, index){
   tryCatch(
     {
@@ -21,6 +28,9 @@ labels_to_string <- function(data, index){
   )
 }
 
+#' Outputs csv documentation for a dataset incluing question_code, question_label and question_labels
+#' 
+#' @param data SPSS dataset
 
 documentation <- function(data){
   output_path <- file.path(Sys.getenv("USERPROFILE"), "Documents/Lemanique Panel/Wave_1", "documentation_wave1.csv")
@@ -44,10 +54,99 @@ documentation <- function(data){
   
 }
 
-full_data <- function(data){
+#' Outputs whole dataset without labels in a csv file
+#' 
+#' @param data SPSS dataset
+
+full_data <- function(data) {
   output_path <- file.path(Sys.getenv("USERPROFILE"), "Documents/Lemanique Panel/Wave_1", "full_data_wave1.csv")
   write.csv(data, output_path)
 }
+
+#' preprocess participants data
+#' 
+#' @param data SPSS dataset
+
+participants <- function(data){
+  output_path <- "data/wave1/paricipants.tsv"
+
+  NAs <- rep(NA_character_,nrow(data["IDNO"]))
+  
+  participants_code <- data$IDNO
+  pays <- data$Pays
+  group <- data$Groupe
+  gp_age_source <- data$GP_Age_source
+  numero_INSEE <- data$Numéro_INSEE
+  numero_OFS <- data$Numéro_OFS
+  weight <- NAs 
+  titre_source <- NAs 
+  cp_source <- data$CP_source
+  localite_source <- data$Localité_source
+  
+  result <- tibble::tibble(
+                           participants_code =  participants_code, 
+                           pays = pays,
+                           group = group,
+                           gp_age_source = gp_age_source,
+                           numero_INSEE = numero_INSEE,
+                           numero_OFS = numero_OFS,
+                           weight = weight,
+                           titre_source = titre_source,
+                           cp_source = cp_source,
+                           localite_source = localite_source
+                           )
+  missing_names <- c("weight", "titre_source")
+  
+  readr::write_tsv(result, output_path)
+}
+
+questions <- function(data){
+  
+}
+
+#' preprocess survey completion data
+#' 
+#' @param data SPSS dataset
+
+survey_completion <- function(data){
+  output_path <- "data/wave1/survey_completion.tsv"
+  
+  NAs <- rep(NA_character_,nrow(data["IDNO"]))
+  
+  participant_code <- data$IDNO
+  
+  result <- tibble::tibble(
+                          participant_code = participant_code,
+                          count_miss1 = NAs,
+                          count_miss2 = NAs,
+                          progress = NAs,
+                          start_date = NAs,
+                          end_date = NAs,
+                          temps_minute = NAs,
+                          flag_troll = NAs
+  )
+  missing_names <- c("count_miss1", "count_miss2", "progress", "start_date", "end_date", "temps_minute", "flag_troll")
+  
+  readr::write_tsv(result, output_path)
+}
+
+#' outputs tsv file containing questions an their label
+#' 
+#' @param data SPSS dataset
+#' @param cols list of all columns to be processed
+#' @param name string containing name of the output tsv file
+
+something_label <- function(data, cols, name){
+  output_path <- paste("data/wave1/",name,"_labels.tsv",sep="")
+  
+  cols_selected <- data |>
+    dplyr::select(tidyselect::all_of(cols))
+  
+  result <- get_labels(cols_selected)
+  readr::write_tsv(result, output_path)
+}
+
+#' reads dataset and calls specific documentation functions
 
 main <- function(){
   
@@ -64,9 +163,26 @@ main <- function(){
   
   wave1_data <- haven::read_sav(file.path(folder, file))
   
-  documentation(wave1_data)
+  #documentation(wave1_data)
   
-  full_data(wave1_data)
+  #full_data(wave1_data)
+  
+  if (!dir.exists(here::here("data/wave1"))) {
+    dir.create(here::here("data/wave1"), recursive = TRUE)
+  }
+  
+  personnes_columns <- c("IDNO","Pays","Groupe","GP_Age_source","Numéro_INSEE","Numéro_OFS","CP_source","Localité_source")
+  survey_completion_columns <- c("IDNO")
+  questions_columns <- filter(wave1_data, str_detect(colnames(wave1_data), "^Q\\d*.*$")) # TODO
+  sections <- c("Permis, véhicules et abbonements", "Multi-résidence", "Pratiques et moyens de déplacements", "Loop Leman Express", "Mobilités pour le travail et études", "Stationnement et financement", "Subventionnement abo", "Mobilités hors du travail", 
+                "excursions", "Séjours", "Opinions", "Variables sociodémographiques 1/2", "Variabes sociodémographiques 2/2", "Variables sociodémographiques_ménage", "Opt-out", "End of questionnaire")
+  sections_start <- c(0, 12, 30, 37, 240/40, 61, 75, 77, 81, 85, 90, 241/107, 109, 115, 123, 129)
+  
+  participants(wave1_data)
+  something_label(wave1_data,personnes_columns,"participants")
+  survey_completion(wave1_data)
+  something_label(wave1_data, survey_completion_columns, "survey_completion")
+  
 }
 
 
